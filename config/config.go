c247177d8e82ecd56c381eba46e348c1ea7bfda5
NikitaSmall <nikitasosnov@yahoo.com>
2019-10-05 11:59:14 +0200

    add url query

--- config/config.go
+++ config/config.go
@@ -1,4 +1,8 @@
 package config
 
-// NewsAPIKey is an API key that allows to get fresh news
-const NewsAPIKey = "5d4fd18fec7f440bbe5b346385dcbbbe"
+const (
+	// NewsAPIKey is an API key that allows to get fresh news
+	NewsAPIKey = "5d4fd18fec7f440bbe5b346385dcbbbe"
+	// NewsAPIDomain is a place where news can be grabbed from
+	NewsAPIDomain = "https://newsapi.org"
+)
---
+++ query/newsApiUrlQuery.go
@@ -0,0 +1,25 @@
+package query
+
+import "fmt"
+
+// NewsAPIURLQuery holds the data in order to create correct URL
+type NewsAPIURLQuery struct {
+	Domain string
+	Query  string
+	APIKey string
+}
+
+// NewNewsAPIURLQuery creates an object that is used as a URL builder
+func NewNewsAPIURLQuery(domain, query, apiKey string) *NewsAPIURLQuery {
+	return &NewsAPIURLQuery{
+		Domain: domain,
+		Query:  query,
+		APIKey: apiKey,
+	}
+}
+
+// GetURL returns a ready to use URL with correct params
+func (query NewsAPIURLQuery) GetURL() string {
+	return fmt.Sprintf("%s/v2/everything?q=%s&apiKey=%s",
+		query.Domain, query.Query, query.APIKey)
+}
